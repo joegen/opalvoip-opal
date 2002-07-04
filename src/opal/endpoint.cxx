e7c6b4a4ba8b2975f5e1990b2a2b89bfa7719714
robertj <robertj@023b2edf-31b2-4de3-b41e-bca80c47788f>
2002-07-04 07:41:47 +0000

    svnimporter: adding tags and branches to revision 515

---
+++ src/opal/endpoint.cxx
@@ -0,0 +1,356 @@
+/*
+ * endpoint.cxx
+ *
+ * Media channels abstraction
+ *
+ * Open Phone Abstraction Library (OPAL)
+ * Formally known as the Open H323 project.
+ *
+ * Copyright (c) 2001 Equivalence Pty. Ltd.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Open Phone Abstraction Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Contributor(s): ______________________________________.
+ *
+ * $Log: endpoint.cxx,v $
+ * Revision 1.2014  2002/07/04 07:41:47  robertj
+ * Fixed memory/thread leak of transports.
+ *
+ * Revision 2.12  2002/07/01 04:56:33  robertj
+ * Updated to OpenH323 v1.9.1
+ *
+ * Revision 2.11  2002/06/16 02:24:43  robertj
+ * Fixed memory leak of failed listeners, thanks Ted Szoczei
+ *
+ * Revision 2.10  2002/04/05 10:37:46  robertj
+ * Rearranged OnRelease to remove the connection from endpoints connection
+ *   list at the end of the release phase rather than the beginning.
+ *
+ * Revision 2.9  2002/01/22 05:12:27  robertj
+ * Revamp of user input API triggered by RFC2833 support
+ *
+ * Revision 2.8  2001/11/14 01:31:55  robertj
+ * Corrected placement of adjusting media format list.
+ *
+ * Revision 2.7  2001/11/13 04:29:48  robertj
+ * Changed OpalTransportAddress CreateTransport and CreateListsner functions
+ *   to have extra parameter to control local binding of sockets.
+ *
+ * Revision 2.6  2001/08/23 05:51:17  robertj
+ * Completed implementation of codec reordering.
+ *
+ * Revision 2.5  2001/08/22 10:20:09  robertj
+ * Changed connection locking to use double mutex to guarantee that
+ *   no threads can ever deadlock or access deleted connection.
+ *
+ * Revision 2.4  2001/08/17 08:26:26  robertj
+ * Moved call end reasons enum from OpalConnection to global.
+ *
+ * Revision 2.3  2001/08/01 05:44:40  robertj
+ * Added function to get all media formats possible in a call.
+ *
+ * Revision 2.2  2001/07/30 07:22:25  robertj
+ * Abstracted listener management from H.323 to OpalEndPoint class.
+ *
+ * Revision 2.1  2001/07/30 01:40:01  robertj
+ * Fixed GNU C++ warnings.
+ *
+ * Revision 2.0  2001/07/27 15:48:25  robertj
+ * Conversion of OpenH323 to Open Phone Abstraction Library (OPAL)
+ *
+ */
+
+#include <ptlib.h>
+
+#ifdef __GNUC__
+#pragma implementation "endpoint.h"
+#endif
+
+#include <opal/endpoint.h>
+
+#include <opal/manager.h>
+#include <opal/call.h>
+
+
+#define new PNEW
+
+
+/////////////////////////////////////////////////////////////////////////////
+
+OpalEndPoint::OpalEndPoint(OpalManager & mgr,
+                           const PCaselessString & prefix,
+                           unsigned attributes)
+  : manager(mgr),
+    prefixName(prefix),
+    attributeBits(attributes)
+{
+  manager.AttachEndPoint(this);
+
+  defaultSignalPort = 0;
+  initialBandwidth = UINT_MAX; // Infinite bandwidth
+
+  connectionsActive.DisallowDeleteObjects();
+
+  PTRACE(3, "OpalEP\tCreated endpoint: " << prefixName);
+}
+
+
+OpalEndPoint::~OpalEndPoint()
+{
+  PTRACE(3, "OpalEP\t" << prefixName << " endpoint destroyed.");
+}
+
+
+void OpalEndPoint::PrintOn(ostream & strm) const
+{
+  strm << "EP<" << prefixName << '>';
+}
+
+
+BOOL OpalEndPoint::StartListeners(const PStringArray & interfaces)
+{
+  BOOL startedOne = FALSE;
+
+  for (PINDEX i = 0; i < interfaces.GetSize(); i++) {
+    OpalTransportAddress iface(interfaces[i], defaultSignalPort);
+    if (StartListener(iface))
+      startedOne = TRUE;
+  }
+
+  return startedOne;
+}
+
+
+BOOL OpalEndPoint::StartListener(const OpalTransportAddress & iface)
+{
+  OpalListener * listener;
+
+  if (iface.IsEmpty())
+    listener = new OpalListenerTCP(*this, INADDR_ANY, defaultSignalPort);
+  else {
+    listener = iface.CreateListener(*this, OpalTransportAddress::FullTSAP);
+    if (listener == NULL) {
+      PTRACE(1, "OpalEP\tCould not create listener: " << iface);
+      return FALSE;
+    }
+  }
+
+  if (StartListener(listener))
+    return TRUE;
+
+  PTRACE(1, "OpalEP\tCould not start listener: " << iface);
+  return FALSE;
+}
+
+
+BOOL OpalEndPoint::StartListener(OpalListener * listener)
+{
+  if (listener == NULL)
+    return FALSE;
+
+  // as the listener is not open, this will have the effect of immediately
+  // stopping the listener thread. This is good - it means that the 
+  // listener Close function will appear to have stopped the thread
+  if (!listener->Open(PCREATE_NOTIFIER(ListenerCallback))) {
+    delete listener;
+    return FALSE;
+  }
+
+  listeners.Append(listener);
+  return TRUE;
+}
+
+
+BOOL OpalEndPoint::RemoveListener(OpalListener * listener)
+{
+  if (listener != NULL)
+    return listeners.Remove(listener);
+
+  listeners.RemoveAll();
+  return TRUE;
+}
+
+
+void OpalEndPoint::ListenerCallback(PThread &, INT param)
+{
+  // Error in accept, usually means listener thread stopped, so just exit
+  if (param == 0)
+    return;
+
+  OpalTransport * transport = (OpalTransport *)param;
+  if (NewIncomingConnection(transport))
+    delete transport;
+}
+
+
+BOOL OpalEndPoint::NewIncomingConnection(OpalTransport * /*transport*/)
+{
+  return TRUE;
+}
+
+
+OpalConnection * OpalEndPoint::GetConnectionWithLock(const PString & token)
+{
+  PWaitAndSignal wait(inUseFlag);
+
+  OpalConnection * connection = connectionsActive.GetAt(token);
+  if (connection == NULL)
+    return NULL;
+
+  if (connection->Lock())
+    return connection;
+
+  return NULL;
+}
+
+
+PStringList OpalEndPoint::GetAllConnections()
+{
+  PStringList tokens;
+
+  inUseFlag.Wait();
+
+  for (PINDEX i = 0; i < connectionsActive.GetSize(); i++)
+    tokens.AppendString(connectionsActive.GetKeyAt(i));
+
+  inUseFlag.Signal();
+
+  return tokens;
+}
+
+
+BOOL OpalEndPoint::HasConnection(const PString & token)
+{
+  PWaitAndSignal wait(inUseFlag);
+  return connectionsActive.Contains(token);
+}
+
+
+BOOL OpalEndPoint::OnIncomingConnection(OpalConnection & connection)
+{
+  return manager.OnIncomingConnection(connection);
+}
+
+
+void OpalEndPoint::OnAlerting(OpalConnection & connection)
+{
+  manager.OnAlerting(connection);
+}
+
+
+void OpalEndPoint::OnConnected(OpalConnection & connection)
+{
+  manager.OnConnected(connection);
+}
+
+
+void OpalEndPoint::OnEstablished(OpalConnection & /*connection*/)
+{
+}
+
+
+BOOL OpalEndPoint::OnReleased(OpalConnection & connection)
+{
+  inUseFlag.Wait();
+  connectionsActive.SetAt(connection.GetToken(), NULL);
+  inUseFlag.Signal();
+
+  return manager.OnReleased(connection);
+}
+
+
+BOOL OpalEndPoint::ClearCall(const PString & token,
+                             OpalCallEndReason reason,
+                             PSyncPoint * sync)
+{
+  return manager.ClearCall(token, reason, sync);
+}
+
+
+BOOL OpalEndPoint::ClearCallSynchronous(const PString & token,
+                                        OpalCallEndReason reason,
+                                        PSyncPoint * sync)
+{
+  PSyncPoint syncPoint;
+  if (sync == NULL)
+    sync = &syncPoint;
+  return manager.ClearCall(token, reason, sync);
+}
+
+
+void OpalEndPoint::ClearAllCalls(OpalCallEndReason reason, BOOL wait)
+{
+  inUseFlag.Wait();
+
+  // Add all connections to the to be deleted set
+  PINDEX i;
+  for (i = 0; i < connectionsActive.GetSize(); i++)
+    manager.ClearCall(connectionsActive.GetKeyAt(i), reason);
+
+  inUseFlag.Signal();
+
+  if (wait)
+    allConnectionsCleared.Wait();
+}
+
+
+void OpalEndPoint::AdjustMediaFormats(const OpalConnection & connection,
+                                      OpalMediaFormatList & mediaFormats) const
+{
+  manager.AdjustMediaFormats(connection, mediaFormats);
+}
+
+
+BOOL OpalEndPoint::OnOpenMediaStream(OpalConnection & connection,
+                                     OpalMediaStream & stream)
+{
+  return manager.OnOpenMediaStream(connection, stream);
+}
+
+
+void OpalEndPoint::OnClosedMediaStream(const OpalMediaStream & stream)
+{
+  manager.OnClosedMediaStream(stream);
+}
+
+
+void OpalEndPoint::OnUserInputString(OpalConnection & connection,
+                                     const PString & value)
+{
+  manager.OnUserInputString(connection, value);
+}
+
+
+void OpalEndPoint::OnUserInputTone(OpalConnection & connection,
+                                   char tone,
+                                   int duration)
+{
+  manager.OnUserInputTone(connection, tone, duration);
+}
+
+
+OpalT120Protocol * OpalEndPoint::CreateT120ProtocolHandler(const OpalConnection & connection) const
+{
+  return manager.CreateT120ProtocolHandler(connection);
+}
+
+
+OpalT38Protocol * OpalEndPoint::CreateT38ProtocolHandler(const OpalConnection & connection) const
+{
+  return manager.CreateT38ProtocolHandler(connection);
+}
+
+
+/////////////////////////////////////////////////////////////////////////////
---
+++ src/sip/sipep.cxx
@@ -0,0 +1,303 @@
+/*
+ * sipep.cxx
+ *
+ * Session Initiation Protocol endpoint.
+ *
+ * Open Phone Abstraction Library (OPAL)
+ *
+ * Copyright (c) 2000 Equivalence Pty. Ltd.
+ *
+ * The contents of this file are subject to the Mozilla Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://www.mozilla.org/MPL/
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See
+ * the License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * The Original Code is Open Phone Abstraction Library.
+ *
+ * The Initial Developer of the Original Code is Equivalence Pty. Ltd.
+ *
+ * Contributor(s): ______________________________________.
+ *
+ * $Log: sipep.cxx,v $
+ * Revision 1.2007  2002/07/04 07:41:47  robertj
+ * Fixed memory/thread leak of transports.
+ *
+ * Revision 2.5  2002/04/10 03:15:17  robertj
+ * Fixed using incorrect field as default reply address on receiving a respons PDU.
+ *
+ * Revision 2.4  2002/04/09 08:04:01  robertj
+ * Fixed typo in previous patch!
+ *
+ * Revision 2.3  2002/04/09 07:18:33  robertj
+ * Fixed the default return address for PDU requests not attached to connection.
+ *
+ * Revision 2.2  2002/04/05 10:42:04  robertj
+ * Major changes to support transactions (UDP timeouts and retries).
+ *
+ * Revision 2.1  2002/02/01 04:53:01  robertj
+ * Added (very primitive!) SIP support.
+ *
+ */
+
+#include <ptlib.h>
+
+#ifdef __GNUC__
+#pragma implementation "sipep.h"
+#endif
+
+#include <sip/sipep.h>
+
+#include <sip/sippdu.h>
+#include <sip/sipcon.h>
+
+#include <opal/manager.h>
+
+
+#define new PNEW
+
+
+////////////////////////////////////////////////////////////////////////////
+
+SIPEndPoint::SIPEndPoint(OpalManager & mgr)
+  : OpalEndPoint(mgr, "sip", CanTerminateCall),
+    registrationID(OpalGloballyUniqueID().AsString()),
+    registrationName(PProcess::Current().GetUserName()),
+    retryTimeoutMin(500),     // 0.5 seconds
+    retryTimeoutMax(0, 4),    // 4 seconds
+    nonInviteTimeout(0, 16),  // 16 seconds
+    pduCleanUpTimeout(0, 5),  // 5 seconds
+    inviteTimeout(0, 32),     // 32 seconds
+    ackTimeout(0, 32)         // 32 seconds
+{
+  defaultSignalPort = 5060;
+  mimeForm = FALSE;
+  maxRetries = 10;
+  lastSentCSeq = 0;
+
+  PTRACE(3, "SIP\tCreated endpoint.");
+}
+
+
+SIPEndPoint::~SIPEndPoint()
+{
+  // Shut down the listeners as soon as possible to avoid race conditions
+  listeners.RemoveAll();
+
+  PTRACE(3, "SIP\tDeleted endpoint.");
+}
+
+
+BOOL SIPEndPoint::NewIncomingConnection(OpalTransport * transport)
+{
+  PTRACE_IF(2, transport->IsReliable(), "SIP\tListening thread started.");
+
+  transport->SetBufferSize(SIP_PDU::MaxSize);
+
+  do {
+    HandlePDU(*transport);
+  } while (transport->IsOpen() && transport->IsReliable());
+
+  PTRACE_IF(2, transport->IsReliable(), "SIP\tListening thread finished.");
+
+  return TRUE;
+}
+
+
+void SIPEndPoint::HandlePDU(OpalTransport & transport)
+{
+  // create a SIP_PDU structure, then get it to read and process PDU
+  SIP_PDU * pdu = new SIP_PDU;
+
+  PTRACE(4, "SIP\tWaiting for PDU on " << transport);
+  if (pdu->Read(transport)) {
+    if (!transport.IsReliable()) {
+      // Calculate default return address
+      if (pdu->GetMethod() != SIP_PDU::NumMethods) {
+        PString via = pdu->GetMIME().GetVia();
+        transport.SetRemoteAddress(via.Mid(via.FindLast(' ')));
+      }
+    }
+    if (!OnReceivedPDU(transport, pdu))
+      delete pdu;
+  }
+  else if (transport.good()) {
+    PTRACE(1, "SIP\tMalformed request received on " << transport);
+    SIP_PDU response(*pdu, SIP_PDU::Failure_BadRequest);
+    response.Write(transport);
+    delete pdu;
+  }
+}
+
+
+BOOL SIPEndPoint::SetUpConnection(OpalCall & call,
+                                  const PString & remoteParty,
+                                  void * userData)
+{
+  if (remoteParty.Find("sip:") != 0)
+    return FALSE;
+
+  PStringStream callID;
+  OpalGloballyUniqueID id;
+  callID << id << '@' << PIPSocket::GetHostName();
+  SIPConnection * connection = CreateConnection(call, callID, userData, NULL, NULL);
+  if (connection == NULL)
+    return FALSE;
+
+  connection->Lock();
+  AddNewConnection(connection);
+  connection->InitiateCall(remoteParty);
+  connection->Unlock();
+
+  return TRUE;
+}
+
+
+BOOL SIPEndPoint::IsAcceptedAddress(const SIPURL & /*toAddr*/)
+{
+  return TRUE;
+}
+
+
+SIPConnection * SIPEndPoint::CreateConnection(OpalCall & call,
+                                              const PString & token,
+                                              void * /*userData*/,
+                                              OpalTransport * transport,
+                                              SIP_PDU * /*invite*/)
+{
+  return new SIPConnection(call, *this, token, transport);
+}
+
+
+void SIPEndPoint::AddNewConnection(SIPConnection * conn)
+{
+  PWaitAndSignal m(inUseFlag);
+  connectionsActive.SetAt(conn->GetToken(), conn);
+}
+
+
+BOOL SIPEndPoint::OnReceivedPDU(OpalTransport & transport, SIP_PDU * pdu)
+{
+  SIPConnection * connection = GetSIPConnectionWithLock(pdu->GetMIME().GetCallID());
+  if (connection != NULL) {
+    connection->QueuePDU(pdu);
+    connection->Unlock();
+    return TRUE;
+  }
+
+  // An INVITE is a special case
+  switch (pdu->GetMethod()) {
+    case SIP_PDU::Method_INVITE :
+      return OnReceivedINVITE(transport, pdu);
+
+    case SIP_PDU::Method_REGISTER :
+      {
+        SIP_PDU response(*pdu, SIP_PDU::Failure_MethodNotAllowed);
+        response.GetMIME().SetAt("Allow", "INVITE");
+        response.Write(transport);
+      }
+      break;
+
+    default :
+      {
+        SIP_PDU response(*pdu, SIP_PDU::Failure_TransactionDoesNotExist);
+        response.Write(transport);
+      }
+  }
+
+  return FALSE;
+}
+
+
+BOOL SIPEndPoint::OnReceivedINVITE(OpalTransport & transport, SIP_PDU * request)
+{
+  // send a response just in case this takes a while
+  SIP_PDU trying(*request, SIP_PDU::Information_Trying);
+  trying.Write(transport);
+
+  SIPMIMEInfo & mime = request->GetMIME();
+
+  // parse the incoming To field, and check if we accept incoming calls for this address
+  SIPURL toAddr(mime.GetTo());
+  if (!IsAcceptedAddress(toAddr)) {
+    PTRACE(1, "SIP\tIncoming INVITE from " << request->GetURI() << " for unknown address " << toAddr);
+    SIP_PDU response(*request, SIP_PDU::Failure_NotFound);
+    response.Write(transport);
+    return FALSE;
+  }
+
+  // ask the endpoint for a connection
+  SIPConnection * connection = CreateConnection(*GetManager().CreateCall(),
+                                                mime.GetCallID(), NULL, &transport, request);
+  if (connection == NULL) {
+    PTRACE(2, "SIP\tFailed to create SIPConnection for INVITE from " << request->GetURI() << " for " << toAddr);
+    SIP_PDU response(*request, SIP_PDU::Failure_NotFound);
+    response.Write(transport);
+    return FALSE;
+  }
+
+  // add the connection to the endpoint list
+  AddNewConnection(connection);
+
+  // Get the connection to handle the rest of the INVITE
+  connection->QueuePDU(request);
+  return TRUE;
+}
+
+
+static BOOL WriteREGISTER(OpalTransport & transport, PObject * param)
+{
+  SIPEndPoint & endpoint = *(SIPEndPoint *)param;
+
+  SIPTransaction request(endpoint, transport);
+  SIPURL name(endpoint.GetRegistrationName(),
+              transport.GetLocalAddress(),
+              endpoint.GetDefaultSignalPort());
+  SIPURL contact(name.GetUserName(),
+                 transport.GetLocalAddress(),
+                 endpoint.GetDefaultSignalPort());
+  request.BuildREGISTER(name, contact);
+
+  return request.Start();
+}
+
+
+BOOL SIPEndPoint::Register(const PString & server)
+{
+  if (listeners.IsEmpty())
+    return FALSE;
+
+  OpalTransportAddress address(server, defaultSignalPort, OpalTransportAddress::UdpPrefix);
+
+  OpalTransport * transport = address.CreateTransport(*this, OpalTransportAddress::NoBinding);
+  transport->ConnectTo(address);
+
+  transport->WriteConnect(WriteREGISTER, this);
+
+  SIP_PDU response;
+  transport->SetReadTimeout(15000);
+  if (!response.Read(*transport)) {
+    delete transport;
+    return FALSE;
+  }
+
+  transport->SetReadTimeout(PMaxTimeInterval);
+  transport->EndConnect(transport->GetLocalAddress());
+
+  PTRACE(2, "SIP\tSelecting reply from " << *transport);
+
+  while (response.GetStatusCode() < SIP_PDU::Successful_OK) {
+    if (!response.Read(*transport))
+      break;
+  }
+
+  delete transport;
+  return response.GetStatusCode() == SIP_PDU::Successful_OK;
+}
+
+
+// End of file ////////////////////////////////////////////////////////////////
