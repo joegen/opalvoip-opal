ec40ccd4c4db165bf11118a15d66dfc2de33f4bb
Craig Southeren <craigs@postincrement.com>
2006-01-27 05:07:13 +0000

    Backports from CVS head

--- samples/simple/main.h
+++ samples/simple/main.h
@@ -22,7 +22,14 @@
  * Contributor(s): ______________________________________.
  *
  * $Log: main.h,v $
- * Revision 1.2018  2005/09/06 04:58:42  dereksmithies
+ * Revision 1.2018.2.1  2006/01/27 05:07:13  csoutheren
+ * Backports from CVS head
+ *
+ * Revision 2.18  2006/01/23 22:56:57  csoutheren
+ * Added 2 second pause before dialling outgoing SIP calls from command line args when
+ *  registrar used
+ *
+ * Revision 2.17  2005/09/06 04:58:42  dereksmithies
  * Add console input options. This is an initial release, and some "refinement"
  * help immensely.
  *
@@ -178,6 +185,8 @@ class MyManager : public OpalManager
 #if P_EXPAT
     OpalIVREndPoint  * ivrEP;
 #endif
+
+    BOOL pauseBeforeDialing;
 
     void HangupCurrentCall();
     void ListSpeedDials();
