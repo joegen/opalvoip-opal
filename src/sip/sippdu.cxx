a52169db341241392bd14bece5b0de8e9f142f54
dsandras <dsandras@023b2edf-31b2-4de3-b41e-bca80c47788f>
2006-03-23 21:29:37 +0000

    Backport from HEAD.

--- src/sip/sippdu.cxx
+++ src/sip/sippdu.cxx
@@ -24,7 +24,14 @@
  * Contributor(s): ______________________________________.
  *
  * $Log: sippdu.cxx,v $
- * Revision 1.2084.2.4  2006/03/19 19:39:56  dsandras
+ * Revision 1.2084.2.5  2006/03/23 21:29:37  dsandras
+ * Backport from HEAD.
+ *
+ * Revision 2.91  2006/03/23 21:25:14  dsandras
+ * Fixed parameter of callback called on registration timeout.
+ * Simplified SIPOptions code.
+ *
+ * Revision 2.83.2.4  2006/03/19 19:39:56  dsandras
  * Backports from HEAD.
  *
  * Revision 2.89  2006/03/19 19:38:25  dsandras
@@ -2120,11 +2127,11 @@ void SIPTransaction::SetTerminated(States newState)
       SIPURL url (GetMIME().GetFrom ());
       PString hosturl;
       // skip transport identifier
-      PINDEX pos = url.GetHostAddress().Find('$');
+      PINDEX pos = url.GetHostName().Find('$');
       if (pos != P_MAX_INDEX)
-	hosturl = url.GetHostAddress().Mid(pos+1);
+	hosturl = url.GetHostName().Mid(pos+1);
       else
-	hosturl = url.GetHostAddress();
+	hosturl = url.GetHostName();
       endpoint.OnRegistrationFailed(hosturl, 
 				    url.GetUserName(),
 				    SIP_PDU::Failure_RequestTimeout,
@@ -2134,8 +2141,7 @@ void SIPTransaction::SetTerminated(States newState)
 
       SIPURL url (GetMIME().GetTo ());
 
-      endpoint.OnMessageFailed(url,
-			       SIP_PDU::Failure_RequestTimeout);
+      endpoint.OnMessageFailed(url, SIP_PDU::Failure_RequestTimeout);
     }
   }
 
@@ -2363,6 +2369,7 @@ SIPOptions::SIPOptions(SIPEndPoint & ep,
   : SIPTransaction(ep, trans)
 {
   PString requestURI;
+  PString hosturl;
   PString id = OpalGloballyUniqueID().AsString() + "@" + PIPSocket::GetHostName();
   OpalTransportAddress viaAddress = ep.GetLocalURL(transport).GetHostAddress();
     
@@ -2382,10 +2389,8 @@ SIPOptions::SIPOptions(SIPEndPoint & ep,
     localName = ep.GetDefaultLocalPartyName ();
 
   SIPURL myAddress("\"" + displayName + "\" <" + localName + "@" + domain + ">"); 
-  if (!address.GetUserName())
-    requestURI = "sip:" + address.GetHostName();
-  else
-    requestURI = "sip:"+address.GetUserName()+"@"+address.GetHostName(),
+
+  requestURI = "sip:" + address.AsQuotedString();
   
   SIP_PDU::Construct(Method_OPTIONS,
 		     requestURI,
