b3b3ae68e34cbad8cc310fb0944b6224b966dc65
Craig Southeren <craigs@postincrement.com>
2007-11-27 04:55:27 +0000

    Backport fixes from revision v3_0_capfix between 18931 and 18935

--- src/h323/h323caps.cxx
+++ src/h323/h323caps.cxx
@@ -601,13 +601,13 @@ BOOL H323Capability::IsUsable(const H323Connection &) const
 
 OpalMediaFormat H323Capability::GetMediaFormat() const
 {
-  return mediaFormat.IsEmpty() ? OpalMediaFormat(GetFormatName()) : mediaFormat;
+  return mediaFormat.IsValid() ? mediaFormat : OpalMediaFormat(GetFormatName());
 }
 
 
 OpalMediaFormat & H323Capability::GetWritableMediaFormat()
 {
-  if (mediaFormat.IsEmpty())
+  if (!mediaFormat.IsValid())
     mediaFormat = GetFormatName();
   return mediaFormat;
 }
@@ -2368,6 +2368,24 @@ static BOOL MatchWildcard(const PCaselessString & str, const PStringArray & wild
   }
 
   return last == str.GetLength();
+}
+
+
+PINDEX H323Capabilities::AddMediaFormat(PINDEX descriptorNum,
+                                        PINDEX simultaneous,
+                                        const OpalMediaFormat & mediaFormat)
+{
+  PINDEX reply = descriptorNum == P_MAX_INDEX ? P_MAX_INDEX : simultaneous;
+
+  if (FindCapability(mediaFormat, H323Capability::e_Unknown, true) == NULL) {
+    H323Capability * capability = H323Capability::Create(mediaFormat);
+    if (capability != NULL) {
+      capability->GetWritableMediaFormat() = mediaFormat;
+      reply = SetCapability(descriptorNum, simultaneous, capability);
+    }
+  }
+
+  return reply;
 }
 
 
