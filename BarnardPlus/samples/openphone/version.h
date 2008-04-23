906d0f9fe70ddd8411689b3a645bd1bbdefc46c5
Robert Jongbloed <robertj@voxlucida.com.au>
2008-04-23 00:52:25 +0000

    Pulled T.38 Media Format out of T.38 protocol specific source files, so if OPAL_T38FAX is disabled, T.38 fax is still available via external means.

--- BarnardPlus/samples/openphone/version.h
+++ BarnardPlus/samples/openphone/version.h
@@ -38,10 +38,10 @@
 #define	COPYRIGHT_HOLDER	"Vox Lucida (Australia)"
 
 
-#define MAJOR_VERSION 2
-#define MINOR_VERSION 1
-#define BUILD_TYPE    AlphaCode
-#define BUILD_NUMBER 0
+#define MAJOR_VERSION 3
+#define MINOR_VERSION 3
+#define BUILD_TYPE    BetaCode
+#define BUILD_NUMBER 1
 
 
 #endif  // _OpenPhone_VERSION_H
