d50be678b0c30f1a0d576a167157dc5571dd8ca4
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2008-09-22 03:13:07 +0000

    Align OpenPhone version with PTLib/OPAL version

--- samples/openphone/version.h
+++ samples/openphone/version.h
@@ -40,8 +40,8 @@
 
 #define MAJOR_VERSION 2
 #define MINOR_VERSION 4
-#define BUILD_TYPE    BetaCode
-#define BUILD_NUMBER 0
+#define BUILD_TYPE    ReleaseCode
+#define BUILD_NUMBER 1
 
 
 #endif  // _OpenPhone_VERSION_H
