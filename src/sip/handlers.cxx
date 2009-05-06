13da8cf8d83b9d5ad9fdac948080ffe8bc1f53d3
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2009-05-06 05:04:07 +0000

    Added std:: to map<> calls for Solaris build compatibility, thanks Bruce McAlister.

--- src/sip/handlers.cxx
+++ src/sip/handlers.cxx
@@ -1089,7 +1089,7 @@ public:
          << m_dialogNotifyVersion++ << "\" state=\"partial\" entity=\""
          << handler.GetAddressOfRecord() << "\">\r\n";
 
-    map<PString, SIPDialogNotification>::iterator iter;
+    std::map<PString, SIPDialogNotification>::iterator iter;
 
     const SIPDialogNotification * info = dynamic_cast<const SIPDialogNotification *>(data);
     if (info != NULL) {
@@ -1112,7 +1112,7 @@ public:
   }
 
   unsigned m_dialogNotifyVersion;
-  map<PString, SIPDialogNotification> m_activeDialogs;
+  std::map<PString, SIPDialogNotification> m_activeDialogs;
 };
 
 static SIPEventPackageFactory::Worker<SIPDialogEventPackageHandler> dialogEventPackageHandler(SIPSubscribe::Dialog);
