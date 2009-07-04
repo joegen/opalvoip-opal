39c712f2458c2f29e641e71b4c7e06831388c885
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2009-07-04 03:52:25 +0000

    Update release version number to 3.4.6

--- version.h
+++ version.h
@@ -35,9 +35,9 @@
  */
 
 #define MAJOR_VERSION 3
-#define MINOR_VERSION 6
+#define MINOR_VERSION 4
 #define BUILD_TYPE ReleaseCode
-#define BUILD_NUMBER 3
+#define BUILD_NUMBER 6
 
 
 #endif  // _OPAL_VERSION_H
