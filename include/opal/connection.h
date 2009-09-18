c0ca6c47793a5f974c065f21645659751be49d67
Robert Jongbloed <robertj@voxlucida.com.au>
2009-09-18 02:17:05 +0000

    Added extra call end cause enum to distinguish between gatekeeper admission request failing and the gatekeeper explicitly disconnecting the call.

--- include/opal/connection.h
+++ include/opal/connection.h
@@ -363,6 +363,7 @@ class OpalConnection : public PSafeObject
       EndedByNoRingBackTone,    /// Call cleared due to missing ringback tone
       EndedByOutOfService,      /// Call cleared because the line is out of service, 
       EndedByAcceptingCallWaiting, /// Call cleared because another call is answered
+      EndedByGkAdmissionFailed,    /// Call cleared because gatekeeper admission request failed.
       NumCallEndReasons,
 
       EndedWithQ931Code = 0x100  /// Q931 code specified in MS byte
