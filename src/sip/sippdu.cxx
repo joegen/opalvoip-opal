0934c95cce031477e32ca7adb3f6b471cb720030
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2009-11-13 05:45:45 +0000

    Add nonce to comparison for digest authentication, pointed out by Martin Hillmeier

--- src/sip/sippdu.cxx
+++ src/sip/sippdu.cxx
@@ -1460,12 +1460,18 @@ PObject::Comparison SIPDigestAuthentication::Compare(const PObject & other) cons
   if (otherAuth == NULL)
     return LessThan;
 
-  Comparison result = GetAuthRealm().Compare(otherAuth->GetAuthRealm());
+  if (algorithm < otherAuth->algorithm)
+    return LessThan;
+  if (algorithm > otherAuth->algorithm)
+    return GreaterThan;
+
+  Comparison result = authRealm.Compare(otherAuth->authRealm);
   if (result != EqualTo)
     return result;
 
-  if (GetAlgorithm() != otherAuth->GetAlgorithm())
-    return GetAlgorithm() < otherAuth->GetAlgorithm() ? LessThan : GreaterThan;
+  result = nonce.Compare(otherAuth->nonce);
+  if (result != EqualTo)
+    return result;
 
   return SIPAuthentication::Compare(other);
 }
