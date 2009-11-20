1fe18beb965a20cded0f892dd7c58cc657dea4f8
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2009-11-20 05:59:13 +0000

    Fixed timestamp calculation when using fax -> G.711 mode.

--- src/t38/t38proto.cxx
+++ src/t38/t38proto.cxx
@@ -348,7 +348,7 @@ PBoolean OpalFaxMediaStream::ReadPacket(RTP_DataFrame & packet)
     packet.SetPayloadType(RTP_DataFrame::MaxPayloadType);
     packet.SetPayloadSize(len);
     packet.SetTimestamp(timestamp);
-    timestamp += len/16;
+    timestamp += len/2;
 
 #if WRITE_PCM_FILE
     static int file = _open("t38_audio_in.pcm", _O_BINARY | _O_CREAT | _O_TRUNC | _O_WRONLY, _S_IREAD | _S_IWRITE);
