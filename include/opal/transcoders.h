85cbe761346a4f821219201b3e72006389c435bc
Robert Jongbloed <robertj@voxlucida.com.au>
2010-03-23 21:17:19 +0000

    Fixed issue with codecs that have small frame sizes, thanks Ted Szoczei
    Small code clean up to have reasonable sized chunk of common code in a subroutine.

--- include/opal/transcoders.h
+++ include/opal/transcoders.h
@@ -359,9 +359,7 @@ class OpalFramedTranscoder : public OpalTranscoder
       */
     OpalFramedTranscoder(
       const OpalMediaFormat & inputMediaFormat,  ///<  Input media format
-      const OpalMediaFormat & outputMediaFormat, ///<  Output media format
-      PINDEX inputBytesPerFrame,  ///<  Number of bytes in an input frame
-      PINDEX outputBytesPerFrame  ///<  Number of bytes in an output frame
+      const OpalMediaFormat & outputMediaFormat  ///<  Output media format
     );
   //@}
 
@@ -425,9 +423,11 @@ class OpalFramedTranscoder : public OpalTranscoder
   //@}
 
   protected:
-    PINDEX     inputBytesPerFrame;
-    PINDEX     outputBytesPerFrame;
-    PINDEX     maxOutputDataSize;
+    void CalculateSizes();
+
+    PINDEX inputBytesPerFrame;
+    PINDEX outputBytesPerFrame;
+    PINDEX maxOutputDataSize;
 };
 
 
@@ -522,7 +522,7 @@ class OpalEmptyFramedAudioTranscoder : public OpalFramedTranscoder
   PCLASSINFO(OpalEmptyFramedAudioTranscoder, OpalFramedTranscoder);
   public:
     OpalEmptyFramedAudioTranscoder(const char * inFormat, const char * outFormat)
-      : OpalFramedTranscoder(inFormat, outFormat, 100, 100)
+      : OpalFramedTranscoder(inFormat, outFormat)
     {  }
 
     PBoolean ConvertFrame(const BYTE *, PINDEX &, BYTE *, PINDEX &)
