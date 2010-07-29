396a3453bbe1d78609e4b5b7e178fa90395ba27b
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2010-07-29 07:18:34 +0000

    Fixed failure to increment CSeq index in SUBSCRIBE/NOTIFY commands if get a authentication challenge.

--- include/sip/handlers.h
+++ include/sip/handlers.h
@@ -143,7 +143,7 @@ protected:
   void RetryLater(unsigned after);
   PDECLARE_NOTIFIER(PTimer, SIPHandler, OnExpireTimeout);
   static PBoolean WriteSIPHandler(OpalTransport & transport, void * info);
-  bool WriteSIPHandler(OpalTransport & transport);
+  virtual bool WriteSIPHandler(OpalTransport & transport, bool forked);
 
   SIPEndPoint               & endpoint;
 
@@ -242,6 +242,7 @@ public:
 
 protected:
   virtual PBoolean SendRequest(SIPHandler::State state);
+  virtual bool WriteSIPHandler(OpalTransport & transport, bool forked);
   void SendStatus(SIP_PDU::StatusCodes code, State state);
   bool DispatchNOTIFY(SIP_PDU & request, SIP_PDU & response);
 
@@ -284,6 +285,7 @@ public:
 
 protected:
   virtual PBoolean SendRequest(SIPHandler::State state);
+  virtual bool WriteSIPHandler(OpalTransport & transport, bool forked);
 
   SIPEventPackage          m_eventPackage;
   SIPDialogContext         m_dialog;
