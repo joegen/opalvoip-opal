fee2a34408c8b24d2f71437d9802472da178cf90
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2010-08-09 13:33:26 +0000

    Fixed overriding of the Contact header override from string options. If have a string option for the Contact, should use it as is!

--- include/sip/sipcon.h
+++ include/sip/sipcon.h
@@ -74,8 +74,9 @@ class SIPEndPoint;
   */
 #define OPAL_OPT_EXTERNAL_SDP "External-SDP"
 
-#define SIP_HEADER_PREFIX   "SIP-Header:"
-#define SIP_HEADER_REPLACES SIP_HEADER_PREFIX"Replaces"
+#define SIP_HEADER_PREFIX      "SIP-Header:"
+#define SIP_HEADER_REPLACES    SIP_HEADER_PREFIX"Replaces"
+#define SIP_HEADER_CONTACT     SIP_HEADER_PREFIX"Contact"
 
 
 /////////////////////////////////////////////////////////////////////////
