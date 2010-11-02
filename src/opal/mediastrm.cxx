aacc35de7804c57522797344cd44de1524cd12c8
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2010-11-02 09:09:11 +0000

    Fixed OnMediaCommand notifier not being called correctly, in particular the video fast update on picture loss not being sent for incoming calls.

--- src/opal/mediastrm.cxx
+++ src/opal/mediastrm.cxx
@@ -146,9 +146,6 @@ void OpalMediaStream::SetCommandNotifier(const PNotifier & notifier)
   if (!LockReadWrite())
     return;
 
-  if (mediaPatch != NULL)
-    mediaPatch->SetCommandNotifier(notifier, IsSink());
-
   commandNotifier = notifier;
 
   UnlockReadWrite();
