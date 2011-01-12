cd4632bac6771a682044cfffdd14d43e3dffb1d0
Craig Southeren <craigs@postincrement.com>
2011-01-12 08:17:18 +0000

    Backport from head of 25053 and 25055 Fix problem with responses to IM not being matched correctly to original messages Fix crash on exit in IM code

--- include/opal/manager.h
+++ include/opal/manager.h
@@ -1566,10 +1566,10 @@ class OpalManager : public PObject
 
 #ifdef OPAL_HAS_IM
   public:
-    OpalIMManager & GetIMManager() { return m_imManager; }
+    OpalIMManager & GetIMManager() { return *m_imManager; }
 
   protected:
-    OpalIMManager m_imManager;
+    OpalIMManager * m_imManager;
 #endif
 };
 
