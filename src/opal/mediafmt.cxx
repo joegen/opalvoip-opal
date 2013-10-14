d1ae60f75a3bfe7a710c500711d6bd4b2ea05e7c
Robert Jongbloed <robertj@voxlucida.com.au>
2013-10-14 22:46:05 +0000

    Make sure "Content Role" media option to indicate video is "main" or "presentation" is propagated to both sides of source/sink streams.

--- src/opal/mediafmt.cxx
+++ src/opal/mediafmt.cxx
@@ -1630,7 +1630,7 @@ OpalVideoFormatInternal::OpalVideoFormatInternal(const char * fullName,
   };
   AddOption(new OpalMediaOptionEnum(OpalVideoFormat::ContentRoleOption(), false,
                                     RoleEnumerations, PARRAYSIZE(RoleEnumerations),
-                                    OpalMediaOption::NoMerge));
+                                    OpalMediaOption::AlwaysMerge));
 
   AddOption(new OpalMediaOptionUnsigned(OpalVideoFormat::ContentRoleMaskOption(),
                                         false, OpalMediaOption::IntersectionMerge,
