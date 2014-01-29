21c80d7aca5040ecd20955f038833af9d3e22635
Robert Jongbloed <robertj@voxlucida.com.au>
2014-01-29 08:40:26 +0000

    Added code to ignore packets in a video frame (until marker) if there is a packet loss. Pushing an incomplete block of encoded data to FFMPEG has a habit of crashing it.

--- plugins/video/common/ffmpeg.h
+++ plugins/video/common/ffmpeg.h
@@ -115,6 +115,7 @@ class FFMPEGCodec
     size_t           m_alignedInputSize;
     EncodedFrame   * m_fullFrame;
     int              m_errorCount;
+    bool             m_ignorePackets;
 
   public:
     FFMPEGCodec(const char * prefix, EncodedFrame * fullFrame);
