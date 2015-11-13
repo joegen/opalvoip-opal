8213bcf153d526dc0bd5280845cddb8bc3acc01d
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2015-11-13 15:05:35 +0000

    Make sure when pausing a OpalRTPMediaStream we don't continually queue data from the OpalRTPSession.

--- src/rtp/rtp_stream.cxx
+++ src/rtp/rtp_stream.cxx
@@ -214,10 +214,10 @@ bool OpalRTPMediaStream::InternalSetPaused(bool pause, bool fromUser, bool fromP
     return false; // Had not changed
 
   if (IsSource()) {
-    // We make referenced copy of pointer so can't be deleted out from under us
-    OpalMediaPatchPtr mediaPatch = m_mediaPatch;
-    if (mediaPatch != NULL)
-      mediaPatch->EnableJitterBuffer(!pause);
+    if (pause)
+      m_rtpSession.RemoveDataNotifier(m_receiveNotifier);
+    else if (IsOpen())
+      m_rtpSession.AddDataNotifier(100, m_receiveNotifier);
   }
 
   return true;
