4383fd757ee7ebad2e09c59cb9b24ab3202df5d1
rjongbloed <rjongbloed@023b2edf-31b2-4de3-b41e-bca80c47788f>
2016-01-26 11:34:25 +0000

    Changed PChannel last read/write count and last read/write error to use thread local storage which eliminates a race condition in their use (detected by Thread Sanitizer) without creating extra contention. As a rule, reading is OK, as multiple threads reading from a channel is logically unsound and not done, however, multiple thread writing is not uncommon, and this is where strange behaviour (not crashes) is possible.
    Removed PChannel::channelName member variable as not consistently used. Made sure the existing virtual PChannel::GetName() is correct for derived classes now.
    
    git-svn-id: svn://svn.code.sf.net/p/opalvoip/code/opal/trunk@34548 023b2edf-31b2-4de3-b41e-bca80c47788f

--- src/h460/h460_std19.cxx
+++ src/h460/h460_std19.cxx
@@ -520,7 +520,7 @@ PBoolean H46019UDPSocket::InternalWriteTo(const Slice * slices, size_t sliceCoun
 {
   if (sliceCount == 1 && slices[0].GetLength() <= 1) {
     PTRACE(5, "Ignoring old NAT opening packet for " << (m_component == PNatMethod::eComponent_RTP ? "RTP" : "RTCP"));
-    lastWriteCount = 1;
+    SetLastWriteCount(1);
     return true;
   }
 
