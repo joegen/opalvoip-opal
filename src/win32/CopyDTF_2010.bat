0056e99d3cc2de0e8c3149ddb86a904574e2bc63
Robert Jongbloed <robertj@voxlucida.com.au>
2017-06-10 12:04:17 +0100

    Removed support for older Visual Studio versions. It's free, just upgrade!

--- src/win32/CopyDTF_2010.bat
+++
@@ -1,9 +0,0 @@
-copy ..\..\lib\Win32\debug\opald.def opald_2010.dtf
-copy ..\..\lib\Win32\release\opal.def opal_2010.dtf
-copy "..\..\lib\Win32\No Trace\opaln.def" opaln_2010.dtf
-
-copy ..\..\lib\x64\debug\opal64d.def opald_2010_x64.dtf
-copy ..\..\lib\x64\release\opal64.def opal_2010_x64.dtf
-copy "..\..\lib\x64\no trace\opal64n.def" opaln_2010_x64.dtf
-
-pause
--- src/win32/CopyDTF_2012.bat
+++
@@ -1,9 +0,0 @@
-copy ..\..\lib\Win32\debug\opald.def opald_2012_x86.dtf
-copy ..\..\lib\Win32\release\opal.def opal_2012_x86.dtf
-copy "..\..\lib\Win32\No Trace\opaln.def" opaln_2012_x86.dtf
-
-copy ..\..\lib\x64\debug\opal64d.def opald_2012_x64.dtf
-copy ..\..\lib\x64\release\opal64.def opal_2012_x64.dtf
-copy "..\..\lib\x64\no trace\opal64n.def" opaln_2012_x64.dtf
-
-pause
