9b1e3bf6669df5225bfd0e4187278e9c2e67dc6e
Robert Jongbloed <robertj@voxlucida.com.au>
2017-07-09 17:27:06 +0100

    Tidied some trace logs

--- src/sip/sipep.cxx
+++ src/sip/sipep.cxx
@@ -344,7 +344,7 @@ OpalTransportPtr SIPEndPoint::GetTransport(const SIPTransactionOwner & transacto
 
         transport->GetChannel()->SetBufferSize(m_maxSizeUDP);
 
-        PTRACE(4, "Created transport " << *transport << " keepAlive=" << keepAliveType);
+        PTRACE(4, "Created transport " << *transport << ", keepAlive=" << keepAliveType);
         AddTransport(transport, keepAliveType);
       }
     }
