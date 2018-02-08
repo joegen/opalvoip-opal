43456fef93335ae57e0759f9e7b9707e03be5583
Robert Jongbloed <robert.jongbloed@blackboard.com>
2018-02-08 18:29:19 +0000

    Fixed trace log getting correct base address for ICE candidate added via STUN request.

--- src/sdp/ice.cxx
+++ src/sdp/ice.cxx
@@ -451,9 +451,9 @@ bool OpalICEMediaTransport::InternalHandleICE(SubChannels subchannel, const void
           PTRACE(4, "Could not derive the candidate type from priority (" << newCandidate.m_priority << ") in STUN message.");
       }
     }
+    newCandidate.m_baseTransportAddress = ap;
     m_remoteCandidates[subchannel].push_back(newCandidate);
     candidate = &m_remoteCandidates[subchannel].back();
-    candidate->m_baseTransportAddress = ap;
     PTRACE(3, *this << subchannel << ", received STUN request for unknown ICE candidate, adding: " << newCandidate);
   }
 
