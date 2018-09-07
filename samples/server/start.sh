ed7f73b1166ea6c9e65dd487c2113575cdb4cae0
Robert Jongbloed <robertj@voxlucida.com.au>
2018-09-07 14:48:30 +0100

    Fixed OPAL server log file getting overwritten instead of rotated when run from start.sh

--- samples/server/start.sh
+++ samples/server/start.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 export LD_LIBRARY_PATH=/usr/local/lib
-/opt/opalsrv/bin/opalsrv --ini-file /opt/opalsrv/etc/opalsrv.ini --log-file /var/log/opalsrv/opalsrv.log --daemon
+cd /opt/opalsrv/bin
+ulimit -c unlimited
+./opalsrv --ini-file /opt/opalsrv/etc/opalsrv.ini --console --daemon
